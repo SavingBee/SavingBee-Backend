65f8e0f8508d86435455e8d640b3d995cae31c16
JiwonJang01 <fx567849@gmail.com>
2025-08-08 00:19:54 +0900

    Project init

---
+++ src/test/java/com/project/savingbee/SavingbeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.savingbee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingbeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
