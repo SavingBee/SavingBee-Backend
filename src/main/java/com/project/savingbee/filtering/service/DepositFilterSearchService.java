2578acdaafc9bb2f8ef2ce6e2cbbc194b7cbdbb5
JiwonJang01 <fx567849@gmail.com>
2025-08-29 21:25:32 +0900

    refector: filtering response

--- src/main/java/com/project/savingbee/filtering/service/DepositFilterSearchService.java
+++ src/main/java/com/project/savingbee/filtering/service/DepositFilterSearchService.java
@@ -1,7 +1,6 @@
 package com.project.savingbee.filtering.service;
 
 import com.project.savingbee.filtering.dto.DepositFilterRequest;
-import com.project.savingbee.filtering.dto.FilterSearchSummaryResponse;
 import com.project.savingbee.filtering.dto.ProductSummaryResponse;
 import com.project.savingbee.filtering.util.KoreanParsing;
 import lombok.RequiredArgsConstructor;
@@ -20,14 +19,21 @@ public class DepositFilterSearchService {
   /**
    * 필터링 -> 검색어 포함되었는지 확인
    */
-  public FilterSearchSummaryResponse depositFilterWithSearch(DepositFilterRequest request){
+  public ProductSummaryResponse depositFilterWithSearch(DepositFilterRequest request){
     log.info("예금 필터링에 검색어 조건 추가 - 검색어 {}",request.getQ());
 
     // 1. 기존 필터링 수행
     DepositFilterRequest chooseRequest = chooseRequsetFiltering(request);
     Page<ProductSummaryResponse> filteredProducts = depositFilterService.depositFilter(chooseRequest);
+
+    // 2. 검색어 체크 - 검색어가 없으면 필터링 결과만 반환
+    // 3. 검색어 전처리
+    // 4. 필터링된 상품들에서 해당 검색어를 포함하는 상품찾기
+      // 4-1. 검색 결과 있음: 검색 결과만 반환
+      // 4-2. 검색 결과 없음: null 반환
   }
 
-
-
+  private chooseRequsetFiltering(DepositFilterRequest request){
+    //
+  }
 }
