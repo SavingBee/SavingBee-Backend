e7f4c3a8f8997cec22547e199e17f5c26825bc4b
Cho Je-seung <tysc1412@naver.com>
2025-09-01 20:46:07 +0900

    chore : 개발/테스트 용 알림 설정 delete 로직 추가

--- src/main/java/com/project/savingbee/common/repository/ProductAlertSettingRepository.java
+++ src/main/java/com/project/savingbee/common/repository/ProductAlertSettingRepository.java
@@ -4,9 +4,12 @@ import com.project.savingbee.common.entity.ProductAlertSetting;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ProductAlertSettingRepository extends JpaRepository<ProductAlertSetting, Long> {
 
   Optional<ProductAlertSetting> findByUserId(Long userId);
+
+  void deleteByUserId(Long userId);
 }
